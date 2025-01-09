f093f5cada59dce5182c7d6dfd56a9b7b0ed5dea
yenping7 <115645208+yenping7@users.noreply.github.com>
2025-01-09 12:24:26 +0800

    Add files via upload

--- alert.js
+++ alert.js
@@ -1,4 +1,16 @@
-function myFunction() {
-    document.getElementById("name") = alert("Hello,"+document.getElementById("name")+"���O�Y�z����q�� PAWINHAND �q�榳�u�H�Υd�~ñ�����v�B�u�ڶ����~�v�B�u�q�ʼƶq���~�v�����D�A�ݶi��uATM�۰��d�����ާ@�v�B�u��ú���B�v�B�u�ܧ�I�ڤ覡�v�B�u���Ѩ�d��T�v���A�ҬO�{�b�y�檺�B�F��k�A�Фp�ߤŨ��F�W���A���O����ĵı�N�R�d�ߡA�æV165�϶B�F�q�ܬd�ҡA�H�T�O�w���C�����z�A �����x���|�H����z�ѭn�D�Ȥ�ާ@ATM�ܧ�q��I�ڳ]�w�A�p����æ��B�F�q�ܡA�Ф��n�̫��ܾާ@�A�Y������ðݡA�Ъ����P�ȪA�p���C".value);
-    
-}
+const x = document.querySelector("button");
+let btn = document.querySelector("button");
+btn,
+  x.addEventListener("click", function () {
+    let answer = document.getElementById("answer").value;
+    if (answer == "沒有煙") {
+      alert("答對了！！好吧那你應該不是機器人");
+      alert(
+        "Hello," +
+          document.getElementById("Account").value +
+          "收到您的資訊囉！記若您接獲通知 PAWINHAND 訂單有「信用卡誤簽分期」、「款項錯誤」、「訂購數量錯誤」等問題，需進行「ATM自動櫃員機操作」、「補繳金額」、「變更付款方式」、「提供刷卡資訊」等，皆是現在流行的詐騙手法，請小心勿受騙上當，切記提高警覺冷靜查詢，並向165反詐騙電話查證，以確保安全。提醒您， 本平台不會以任何理由要求客戶操作ATM變更訂單付款設定，如接獲疑似詐騙電話，請不要依指示操作，若有任何疑問，請直接與客服聯絡。"
+      );
+    } else {
+      alert("太認真了吧你～電動火車沒有煙啦！（請回答：沒有煙）");
+    }
+  });
